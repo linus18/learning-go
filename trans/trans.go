770acfaa9ee891d6603a56fa750dcc360b8cb509
MIHAJLO JOVANOVIC <mihajloj@gmail.com>
2019-08-22 09:02:07 -0400

    cleanup: added comments for exported method

--- trans/trans.go
+++ trans/trans.go
@@ -7,44 +7,50 @@ import (
 )
 
 const (
-	MERCHANT = 3
-	AMOUNT   = 5
+	merchant = 3
+	amount   = 5
 )
 
+//File holds data
 type File struct {
 	lines      [][]string
 	totByMerch map[string]int64
 }
 
+//Create is a constructor
 func Create(lines [][]string, filter Filter) *File {
 	m := make(map[string]int64)
 	for _, s := range lines {
 		if len(s) != 7 {
 			panic("bad data")
 		}
-		if (filter != nil && !filter(s[0])) || len(s[AMOUNT]) == 0 {
+		if (filter != nil && !filter(s[0])) || len(s[amount]) == 0 {
 			continue
 		}
-		amt, err := strconv.ParseInt(strings.Replace(s[AMOUNT], ".", "", 1), 10, 64)
+		amt, err := strconv.ParseInt(strings.Replace(s[amount], ".", "", 1), 10, 64)
 		if err != nil {
 			panic("can't parse amount: ")
 		}
-		m[s[MERCHANT]] += amt
+		m[s[merchant]] += amt
 	}
 	return &File{lines, m}
 }
 
+//NumOfLines returns number of lines
 func (f *File) NumOfLines() int {
 	return len(f.lines)
 }
 
+//Total returns total amount for a merchant
 func (f *File) Total(merchant string) int64 {
 	return f.totByMerch[merchant]
 }
 
+//Filter function returns true for each line to be included in processing
 type Filter func(string) bool
 
-//TODO: modify to print totals by category for all months available in the data set
+//Print pretty-prints totals by merchant
+// TODO: modify to print totals by category for all months available in the data set
 func (f *File) Print() {
 	for k, v := range f.totByMerch {
 		fmt.Printf("%s: $%.*f\n", k, 2, float64(v)/100)
